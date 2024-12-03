7d5808119032ef4304eda8205105766293f1b68b
LeonardoLpz <leodanlors1993@gmail.com>
2024-12-03 03:36:48 -0600

    first view

--- src/components/LoginGoogle.vue
+++ src/components/LoginGoogle.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <GoogleLogin v-if="!userDetails"
-      :client-id="clientId"
-      :callback="getUserData" 
-      auto-login/>
+  <div class="login-container">
+    <va-card class="login-card">
+      <div class="login-header">
+        <img src="@/assets/Logo-calcifer.png" alt="Logo" class="login-logo" />
+        <h1 class="login-title">Infinity System</h1>
+      </div>
+      <va-card-content>
+        <GoogleLogin v-if="!userDetails"
+        :client-id="clientId"
+        :callback="getUserData" 
+        auto-login/>
       <div v-if="userDetails">
-      <h2>User Details</h2>
-      <p>Name: {{ userDetails.name }}</p>
-      <p>Email: {{ userDetails.email }}</p>
-      <p>Profile Picture: <img
+        <h2>User Details</h2>
+        <p>Name: {{ userDetails.name }}</p>
+        <p>Email: {{ userDetails.email }}</p>
+        <p>Profile Picture: <img
           :src="userDetails.picture"
-          alt="Profile Picture"
-        ></p>
-    </div>
+          alt="Profile Picture"></p>
+        </div>
+      </va-card-content>
+    </va-card>
   </div>
 </template>
 
@@ -40,3 +47,32 @@ export default {
   }
 };
 </script>
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #f5f5f5;
+}
+
+.login-card {
+  width: 400px;
+}
+
+.login-header {
+  text-align: center;
+  margin-bottom: 1.2rem;
+}
+
+.login-logo {
+  margin-top: 1.2rem;
+  width: 100px;
+  height: auto;
+}
+
+.login-title {
+  margin-top: 10px;
+  font-size: 2.2rem;
+}
+</style>
